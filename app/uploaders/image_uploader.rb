c237768e261091a316b69d2b2ed070868dd65a9a
Sashka <sasha.bezverha@gmail.com>
2015-09-30 13:24:56 +0300

    add default avatar

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,12 +15,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  def default_url
+    "default.png"
+  end
   version :thumb do
-    process :resize_to_limit => [50, 50]
+    process :resize_to_limit => [200, 200]
   end
   version :small_thumb, from_version: :thumb do
     process resize_to_fill: [30, 30]
-    end
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
