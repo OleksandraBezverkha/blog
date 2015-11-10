5c5d45e6937cdcd7df559d5ef145d2d83330f57d
Sashka <sasha.bezverha@gmail.com>
2015-11-10 14:31:32 +0200

    heroku mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-
-  config.app_domain = 'fast-garden-4380.herokuapp.com'
-
   # Email
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.default_url_options = { :host => 'https://fast-garden-4380.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
