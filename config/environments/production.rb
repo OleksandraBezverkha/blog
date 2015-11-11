6fb87eda4a91880d4c3eef6a3c8eded62e394d67
Sashka <sasha.bezverha@gmail.com>
2015-11-11 15:26:39 +0200

    heroku facebook email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Email
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = {:host => 'https://fast-garden-4380.herokuapp.com/' }
+  config.action_mailer.default_url_options = {:host => 'fast-garden-4380.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
