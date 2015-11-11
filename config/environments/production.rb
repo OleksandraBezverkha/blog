9ab2b0e0fc92d6decf805de4713b541ee5c47fd4
Sashka <sasha.bezverha@gmail.com>
2015-11-11 15:17:45 +0200

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Email
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.default_url_options = {:host => 'https://fast-garden-4380.herokuapp.com/' }
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
@@ -95,6 +95,6 @@ Rails.application.configure do
       domain: 'gmail.com'
   }
 
-  config.assets.compress = true
+  # config.assets.compress = true
 
 end
